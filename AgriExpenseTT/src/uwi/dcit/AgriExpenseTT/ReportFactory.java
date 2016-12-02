d1e7ab01a65f2b80d90cac8991731b129a25a36b
HyperManTT <randyram9@outlook.com>
2016-12-02 09:28:58 -0400

    Added block comments for new features and old report generation code

--- AgriExpenseTT/src/uwi/dcit/AgriExpenseTT/ReportFactory.java
+++ AgriExpenseTT/src/uwi/dcit/AgriExpenseTT/ReportFactory.java
@@ -8,16 +8,25 @@ import uwi.dcit.AgriExpenseTT.Report.ExcelReport;
  * Created by Randy Ram on 19/11/2016.
  */
 
+
+/**
+ * Factory class used to create all types of reports
+ */
 public class ReportFactory {
 
     Activity activity;
     public ReportFactory(Activity act){
         this.activity = act;
     }
+
+    /**
+     * Create a specific type of report based on the reportType parameter
+     * @param reportType - The type of report that is to be created.
+     */
     public void createReport(String reportType){
         if (reportType.equalsIgnoreCase("EXCEL")){
             ExcelReport report = new ExcelReport(activity);
-            report.createReport();
+            report.createReport();  //Invoke the specific function to create an Excel report
         }
     }
 }
